acb0f8f52a7a8003512171996f5bdc43d883152a
Francis Chen <fncis.chen@gmail.com>
2019-01-29 14:15:27 -0800

    organize code

--- js/index.js
+++ js/index.js
@@ -1,32 +1,64 @@
 // Your code goes here
 
-// drag and drop interaces
-// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
 
-// Event Listeners 1-7
 
-    // Step 1 // Define draggable obj + drag zone. 
 
-        draggable = document.querySelector('.container.home').querySelectorAll(".text-content, .img-content");
+
+// Event Listeners 2-8 // Trip Planning
+
+    // Step 1 // Declare draggable obj + drag zone. 
+
+        draggableObjects = document.querySelector('.container.home').querySelectorAll(".text-content, .img-content");
         dragzone = document.querySelector('.container.home');
 
     // Step 2 // Set up event listeners for draggable & drag zone.
 
+        // helper function
+
+        function makeDraggable(draggable) {
+
+            draggable.addEventListener("drag", onDrag); // 2
+            draggable.addEventListener("dragstart", onDragStart); // 3
+            draggable.addEventListener("dragend", onDragEnd); // 4
+        
+            draggable.addEventListener("dragenter", onDragEnter); // 5
+            draggable.addEventListener("dragleave", onDragLeave); // 6
+
+        };
+
         // draggable 
 
-        draggable.addEventListener("drag"); // 1
-        draggable.addEventListener("dragstart"); // 2
-        draggable.addEventListener("dragend"); // 3
-    
-        draggable.addEventListener("dragenter"); // 4
-        draggable.addEventListener("dragleave"); // 5
-        
+        draggableObjects.forEach(makeDraggable);
+
         // dragzone
 
-        dragzone.addEventListener("drop"); // 6
-        dragzone.addEventListener("dragover"); // 7
+        dragzone.addEventListener("drop", onDrop); // 7
+        dragzone.addEventListener("dragover", onDragOver); // 8
 
     // Step 3 // Declare event handler functions.
 
+        function onDrag(event) {};
+        function onDragStart(event){};
+        function onDragEnd(event){};
+        function onDragEnter(event){};
+        function onDragLeave(event){};
 
-// Event Listeners 8-10
+        function onDrop(event){};
+        function onDragOver(event){};
+
+    // for more info
+    // drag and drop interaces
+    // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+
+
+// Event Listeners 9-10
+
+    // Step 1 // Declare destination buttons.
+
+        destinations = document.querySelector(".content-pick").querySelectorAll(".destination");
+
+    // Step 2 // add event listeners.
+
+
+
+    // Step 3 // Declare event handler functions.
